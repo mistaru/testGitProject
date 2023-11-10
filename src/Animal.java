bce867f928dc71412468f087c1109af41cef262b
argen_kasymov <mistaru@bk.ru>
2023-11-10 19:28:52 +0600

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String name;
+}
+
+
+/*
+*
+*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mistaru/testGitProject.git
+git push -u origin main
+* */
