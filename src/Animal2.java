4e7b496119aba60fc567cded9f8adf207730063a
AizhAn1903 <149721164+AizhAn1903@users.noreply.github.com>
2023-11-24 18:56:55 +0600

    добавил новую ветку

---
+++ src/Animal2.java
@@ -0,0 +1,16 @@
+public class Animal2 {
+    private String name;
+    private Long numberLimbs;
+
+    public Animal2(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
